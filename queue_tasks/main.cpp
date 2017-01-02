71e46595fc1ac1d1a8392d521f012c9586d0f9fc
chudizm <the_show_must_go_on@inbox.ru>
2017-01-02 14:39:11 +0300

    02.01.17 -- trying to fix troubles isEmpty X isFull

--- queue_tasks/main.cpp
+++ queue_tasks/main.cpp
@@ -3,5 +3,13 @@
 void main()
 {
 	queue A(3);
+	A.enq(1); A.enq(2); A.enq(3);
+	/*cout << "Empty?\t" << A.isEmpty() << endl << "Full?\t" << A.isFull() << endl << A.deq() << A.deq() << A.deq() << endl;
+	cout << "Empty?\t" << A.isEmpty() << endl << "Full?\t" << A.isFull() << endl;*/
+	for (int i = 0; i < A.getSize()+1; i++)
+	{
+		cout << i << " interation:\n\n" << "A.isEmpty == " << A.isEmpty() << endl << "A.isFull == " << A.isFull() << endl << endl << "Dequeued: " << A.deq() << endl << endl;
+
+	}
 	system("pause");
 }
--- queue_tasks/queue.cpp
+++ queue_tasks/queue.cpp
@@ -2,26 +2,26 @@
 
 // ------------------------------<РЕАЛИЗАЦИИ>-------------------------------------
 	
-																	// - инициализация
-queue::queue(const unsigned int& _size)
+																	
+queue::queue(const unsigned int& _size)									// - инициализация
 {
 	try
 	{
 		if ((_size < 1) || (_size > MAXSIZE))							// - проверка на корректность размера
-			throw("Incorrect size of queue");
+			throw("\nIncorrect size of queue\n");
 	}
 	catch (char* str)
 	{
 		cout << str;
+		return;
 	}
 	size = _size;
 	start = 0;
 	end = 0;
 	amount = 0;
 	pMem = new int[size + 1];
-}
-																	// - копирование
-queue::queue(const queue& arg)
+}																	
+queue::queue(const queue& arg)										// - копирование
 {
 	size = arg.size;
 	start = arg.start;
@@ -31,8 +31,69 @@ queue::queue(const queue& arg)
 	for (int i = 0; i <= size; i++)
 		pMem[i] = arg.pMem[i];
 }
-																	// - деструктор
-queue::~queue()
+bool queue::isFull()
+{
+	bool res = (amount == size);
+	return res;
+}
+bool queue::isEmpty()
+{
+	bool res = (amount == 0);
+	return res;
+}
+int queue::getSize()														// - получить размер очереди
+{
+	return size;
+}
+void queue::enq(const int& el)											// - добавление элемента в очередь
+{
+	try																	// - проверка наличия места в очереди
+	{
+		if (isFull()) throw("\nNo free space in queue\n");
+	}
+	catch (char* str)
+	{
+		cout << str;
+		return;
+	}
+	pMem[end] = el;
+	end++; 
+	amount++;
+	if (end > size)														// - проверка "кругового" заполнения очереди
+		end = end - size + 1;											// - возвращение end в начало очереди
+}
+int queue::deq()														// - удаление первого в очереди элемента из очереди
+{
+	try																	// - проверка наличия элементов для извлечения
+	{
+		if (isEmpty()) throw("\nNo elements to dequeue\n");
+	}
+	catch (char* str)
+	{
+		cout << str;
+		return NULL;
+	}
+	buf = pMem[start];
+	start++;
+	amount--;
+	if (start > size)													// - проверка кругового заполнения очереди
+		start = start - size + 1;										// - возвращаем start на начало очереди
+	return buf;
+}
+/*int queue::peek()														// - просмотр первого из очереди элемента
+{
+	try																	// - проверка наличия элементов для просмотра
+	{
+		if (isEmpty()) throw("\nNo elements to show\n");
+	}
+	catch (char* str)
+	{
+		cout << str;
+		return NULL;
+	}
+	return pMem[start];
+}*/
+queue::~queue()														// - деструктор
 {
 	delete[] pMem;
 	pMem = NULL;
